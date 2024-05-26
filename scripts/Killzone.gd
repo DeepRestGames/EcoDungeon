cecb5917d61064608dbb9eb5945aec4c893d24ee
Matteo Marcuzzo <matteo.marcuzzo.it@gmail.com>
2024-05-26 20:18:44 +0200

    Placeholder death text

--- scripts/Killzone.gd
+++ scripts/Killzone.gd
@@ -1,15 +1,18 @@
 extends Area3D
 
 @onready var timer = $Timer
+@onready var placeholder_death = %PlaceholderDeath
 
 
 func _on_body_entered(body):
 	Engine.time_scale = 0.5
 	body.get_node("CollisionShape3D").queue_free()
 	timer.start()
+	placeholder_death.visible = true
 
 
 func _on_timer_timeout():
 	Engine.time_scale = 1.0
 	get_tree().reload_current_scene()
+	placeholder_death.visible = false
 
