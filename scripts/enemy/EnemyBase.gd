c8b14d2b55317d6e4ba7e1bb9fc0cd17e77481b4
Marco Dugatto <dughi.boogie@gmail.com>
2024-05-30 23:23:11 +0200

    Enemy behaviours modularity
    Tried adding a bit of modularity to enemies behaviours, to make it easier creating different combinations of behaviours.
    Added enemies that follow other enemies.

--- scripts/enemy/EnemyBase.gd
+++ scripts/enemy/EnemyBase.gd
@@ -7,15 +7,9 @@ extends CharacterBody3D
 ##
 
 
-@onready var nav_agent = $NavigationAgent3D
 @export var SPEED: float = 5.0
 @export var max_hp: int = 5
 var current_hp: int = max_hp
-var target: Node3D = null
-
-
-func update_target(new_target: Node3D):
-	target = new_target
 
 
 func take_damage(damage: int):
