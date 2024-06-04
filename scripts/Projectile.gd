81c9d0b2003144dee66ad849987507033dce8a50
Matteo Marcuzzo <matteo.marcuzzo.it@gmail.com>
2024-06-04 20:18:11 +0200

    Minor fixes

--- scripts/Projectile.gd
+++ scripts/Projectile.gd
@@ -4,7 +4,6 @@ const BULLET_VELOCITY = 20
 const BULLET_DAMAGE = 2
 
 var time_alive = 1.5
-var hit = false
 
 @onready var collision_shape = $CollisionShape3D
 
@@ -13,13 +12,13 @@ func _physics_process(delta):
 	time_alive -= delta
 	if time_alive < 0:
 		destroy()
-	
-	var collision = move_and_collide(-delta * BULLET_VELOCITY * transform.basis.z)
-	if collision:
-		var enemy = collision.get_collider() as EnemyBase
-		if enemy:
-			enemy.take_damage(BULLET_DAMAGE)
-		destroy()
+	else:
+		var collision = move_and_collide(-delta * BULLET_VELOCITY * transform.basis.z)
+		if collision:
+			var enemy = collision.get_collider() as EnemyBase
+			if enemy:
+				enemy.take_damage(BULLET_DAMAGE)
+			destroy()
 
 
 func destroy():
