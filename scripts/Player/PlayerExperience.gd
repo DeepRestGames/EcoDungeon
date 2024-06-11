d55dcea0d505af4ec41cfcde4bed54133a336269
Matteo Marcuzzo <matteo.marcuzzo.it@gmail.com>
2024-06-11 18:22:08 +0200

    Player experience/HP UI

--- scripts/Player/PlayerExperience.gd
+++ scripts/Player/PlayerExperience.gd
@@ -1,3 +1,4 @@
+class_name PlayerExperience
 extends Node
 
 # Note: To balance
@@ -7,14 +8,15 @@ const base_multiplier: float = 1.1
 @onready var xp_to_level: float = 100
 
 signal xp_change(xp)
-signal level_up(level)
+signal level_up(level, overflow_xp)
 
 var current_xp: float = 0:
 	set(value):
 		current_xp = value
 		if current_xp >= xp_to_level:
-			current_xp = 0
+			var overflow = current_xp - xp_to_level
+			current_xp = overflow
 			current_level +=1
 			xp_to_level =  ( xp_to_level + base_value ) * base_multiplier	
-			level_up.emit(current_level)
+			level_up.emit(current_level, overflow)
 		xp_change.emit(current_xp)
--- scripts/Player/UI.gd
+++ scripts/Player/UI.gd
@@ -1,23 +1,40 @@
 extends Control
 
 @export var player: Player
+@export var player_xp: PlayerExperience
 @onready var health = $Health
 @onready var experience = $Experience
 @onready var level_label = $LevelLabel
+@onready var health_label = $Health/HealthLabel
+@onready var xp_label = $Experience/XpLabel
 
 
 func _ready():
+	health.value = player.current_hp
 	health.max_value = player.max_hp
+	set_player_hp_label(player.current_hp, player.max_hp)
+	set_xp_label(player_xp.current_xp, player_xp.xp_to_level)
+	
 
-
-
-func _on_player_experience_level_up(level):
+func _on_player_experience_level_up(level, xp_overflow):
 	level_label.text = "Level %s" % str(level)
+	experience.max_value = player_xp.xp_to_level
+	experience.value = xp_overflow
 
 
 func _on_player_experience_xp_change(xp):
 	experience.value = xp
+	set_xp_label(xp, player_xp.xp_to_level)
+	
 
 
 func _on_player_health_change(hp):
 	health.value = hp
+	set_player_hp_label(hp, player.max_hp)
+	
+func set_player_hp_label(current_value, max_value):
+	health_label.text = "HP: %s/%s" % [str(current_value), str(max_value)]
+	
+func set_xp_label(current_value, max_value):
+	xp_label.text = "XP: %s/%s" % [str(current_value), str(max_value)]
+ 
