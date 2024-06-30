0e657a228375978cc8f70e4fdd9fa0435d8f9612
Marco Dugatto <dughi.boogie@gmail.com>
2024-06-30 16:26:44 +0200

    Small refactoring - Put Wave XML parser into its own class

--- scripts/enemy/EnemySpawner.gd
+++ scripts/enemy/EnemySpawner.gd
@@ -1,23 +1,6 @@
 extends Node3D
 
 
-class EnemyWave:
-	var wave_id: int
-	var wave_duration: float
-	var wave_enemies: Array[EnemyWaveEntry]
-	
-	func print_wave_info():
-		print("--------- Wave info ---------")
-		print("Wave ID: " + str(wave_id))
-		print("Wave duration: " + str(wave_duration))
-		for i in wave_enemies:
-			print("Total of " + str(i.enemy_count) + " of enemy type: " + i.enemy_scene_path)
-
-class EnemyWaveEntry:
-	var enemy_scene_path: String
-	var enemy_count: int
-
-
 # TODO Suit this for multiple enemy scenes
 # Enemy scenes
 var enemy_follow_player_scene = preload("res://scenes/Enemies/EnemyFollowPlayer_02.tscn")
@@ -27,76 +10,17 @@ var enemy_follow_player_scene = preload("res://scenes/Enemies/EnemyFollowPlayer_
 @onready var enemy_spawn_cooldown = $EnemySpawnCooldown
 
 # Enemy waves
-var enemy_waves: Array[EnemyWave]
+var enemy_waves: Array
 var current_wave_id:= -1
 
 
+# TODO Once main menu's level selection signal is done, move enemy_waves initialization to initialize function
 func _ready():
-	_parse_enemy_wave_xml()
-	_on_enemy_spawn_cooldown_timeout()
+	enemy_waves = EnemyWaveXMLParser.new()._parse_enemy_wave_xml("test_enemy_waves")
 
-
-func _parse_enemy_wave_xml():
-	var parser = XMLParser.new()
-	parser.open("res://resources/enemy_waves/test_enemy_waves.xml")
-	
-	# Temporary variables to create wave objects
-	var wave_id: int
-	var wave_duration: float
-	var wave_enemies: Array[EnemyWaveEntry]
-	var wave_enemy_scene: String
-	var wave_enemy_count: int
-	
-	while parser.read() != ERR_FILE_EOF:
-		
-		if parser.get_node_type() == XMLParser.NODE_ELEMENT:
-			if parser.get_node_name() == "wave":
-				wave_id = parser.get_attribute_value(0).to_int()
-				#print("Current wave id: " + str(wave_id))
-				continue
-			
-			if parser.get_node_name() == "wave_duration":
-				parser.read()
-				if parser.get_node_type() == XMLParser.NODE_TEXT:
-					wave_duration = parser.get_node_data().to_float()
-					#print("Current wave duration: " + str(wave_duration))
-				continue
-			
-			if parser.get_node_name() == "enemy_scene":
-				parser.read()
-				wave_enemy_scene = parser.get_node_data()
-				#print("Current wave enemy scene: " + wave_enemy_scene)
-				continue
-			
-			if parser.get_node_name() == "enemy_count":
-				parser.read()
-				wave_enemy_count = parser.get_node_data().to_int()
-				#print("Current wave enemy count: " + str(wave_enemy_count))
-				continue
-		
-		if parser.get_node_type() == XMLParser.NODE_ELEMENT_END:
-			if parser.get_node_name() == "enemy_wave_entry":
-				var enemy_wave_entry = EnemyWaveEntry.new()
-				enemy_wave_entry.enemy_scene_path = wave_enemy_scene
-				enemy_wave_entry.enemy_count = wave_enemy_count
-				wave_enemies.append(enemy_wave_entry)
-				#print("Enemy wave entry saved!")
-				
-				wave_enemy_scene = ""
-				wave_enemy_count = 0
-				continue
-			
-			if parser.get_node_name() == "wave":
-				var enemy_wave = EnemyWave.new()
-				enemy_wave.wave_id = wave_id
-				enemy_wave.wave_duration = wave_duration
-				enemy_wave.wave_enemies = wave_enemies.duplicate(true)
-				enemy_waves.append(enemy_wave)
-				#print("Enemy wave saved!")
-				
-				wave_duration = 0.0
-				wave_enemies.clear()
-				continue
+# TODO Connect this function to main menu level selection signal
+#func initialize_level_enemy_waves(level_id: String):
+	#enemy_waves = EnemyWaveXMLParser.new()._parse_enemy_wave_xml("test_enemy_waves")
 
 
 func _on_enemy_spawn_cooldown_timeout():
@@ -112,7 +36,6 @@ func _on_enemy_spawn_cooldown_timeout():
 	# Just for debugging purposes
 	if current_wave.wave_id != current_wave_id:
 		print("Enemy wave ID mismatch!")
-
 	
 	for enemy_wave_entry in current_wave.wave_enemies:
 		for enemy_index in enemy_wave_entry.enemy_count:
