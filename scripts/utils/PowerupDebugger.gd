6f1ded28030bc0304bbf9c7bb0ad081e82f2d4b5
Marco Dugatto <dughi.boogie@gmail.com>
2024-10-18 21:49:13 +0200

    Powerups can now be added freely
    Still missing list of acquired powerups for clarity

--- scripts/utils/PowerupDebugger.gd
+++ scripts/utils/PowerupDebugger.gd
@@ -1,7 +1,35 @@
 extends Control
 
-
 @onready var debugger_panel = $DebuggerPanel
+@onready var powerup_icon = $DebuggerPanel/PowerupIcon
+@onready var powerup_name = $DebuggerPanel/PowerupName
+@onready var powerup_description = $DebuggerPanel/PowerupDescription
+
+var powerups_directory = "res://resources/powerups/"
+var powerup_resources: Array
+
+var current_powerup: WeaponPowerup:
+	get:
+		return current_powerup
+	set(value):
+		current_powerup = value
+		powerup_icon.texture = current_powerup.powerup_icon
+		powerup_name.text = current_powerup.powerup_name
+		powerup_description.text = current_powerup.powerup_description
+var current_powerup_index = 0
+
+
+func _ready():
+	_get_powerups_resources_from_folder()
+	current_powerup = powerup_resources[current_powerup_index]
+
+
+func _get_powerups_resources_from_folder():
+	var powerups_file_names = DirAccess.get_files_at(powerups_directory)
+	
+	for current_powerup_file_name in powerups_file_names:
+		var current_powerup = load(powerups_directory + current_powerup_file_name)
+		powerup_resources.append(current_powerup)
 
 
 func _on_open_powerup_button_pressed():
@@ -11,3 +39,17 @@ func _on_open_powerup_button_pressed():
 	else:
 		debugger_panel.show()
 		get_tree().paused = true
+
+
+func _on_left_button_pressed():
+	current_powerup_index -= 1
+	current_powerup = powerup_resources[current_powerup_index]
+
+
+func _on_right_button_pressed():
+	current_powerup_index += 1
+	current_powerup = powerup_resources[current_powerup_index]
+
+
+func _on_add_powerup_button_pressed():
+	WeaponPowerupSystem.add_powerup($"../../BaseWeapon", current_powerup)
